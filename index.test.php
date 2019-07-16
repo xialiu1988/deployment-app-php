ac0235758d6b73f47c9f69b9c3f0493ee67485cb
John Cokos <highlander.44@gmail.com>
2019-07-16 00:38:54 -0700

    fix test

--- index.test.php
+++ index.test.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 final class TalkerTest extends TestCase {
 
     public function testCanBeCreatedFromValidText(): void {
-        $msg = new Talker("abxc");
+        $msg = new Talker("abc");
         $this->assertEquals(
             'abc',
             $msg->message
